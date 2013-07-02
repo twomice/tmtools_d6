533a43faf6d82fa6c344427cc3f1a19c2b0e6077
Allen Shaw <allen@emphanos.com>
2013-07-02 10:07:13 -0500

    Initial commit.

---
+++ functions.php
@@ -0,0 +1,82 @@
+<?php
+/*
+ * $Id: functions.php 7 2012-07-26 17:44:43Z as $
+ */
+
+/** Defined in hook_menu, callback to create module settings form
+ */
+function _tmtools_settings() {
+  $form['tm_maintenance_time'] = array(
+    '#type' => 'textfield',
+    '#title' => t('Maintenance shutdown time'),
+    '#default_value' => variable_get('maintenance_time', ''),
+    '#description' => t('A time (less than 24 hours in the future) at which you intent to shut down this site for maintenance; should be in server time zone ('. date('T') .'). Format: "HH:MM".'),
+    '#element_validate' => array('_validate_setting_shutdown_time'),
+  );
+  return system_settings_form($form);
+}
+
+/**
+ * Check that a value is a 24-hour time between now and midnight in format HH:MM
+ * @param array $element Drupal form element
+ * @param array $form_state Drupal form state
+ * @return bool TRUE on success, FALSE on failure
+ */
+function _validate_setting_shutdown_time($element, $form_state) {
+  if (empty($element['#value'])) {
+    return true;
+  }
+  if (! preg_match('/[012][0-9]:[0-9]{2}/', $element['#value'])) {
+    form_error($element, 'Must be in 24-hour time format MM:HH');
+  }
+
+  if (time() > strtotime($element['#value'])) {
+    form_error($element, 'Time must be between now and midnight tonight.');
+  }
+  return FALSE;
+}
+
+/**
+ * Helper function to get parsed query without running it. (Modified code
+ * from db_query().
+ */
+function tm_queryp($query) {
+
+  $args = func_get_args();
+  array_shift($args);
+  $query = db_prefix_tables($query);
+  if (isset($args[0]) and is_array($args[0])) { // 'All arguments in one array' syntax
+  $args = $args[0];
+  }
+  _db_query_callback($args, TRUE);
+  $query = preg_replace_callback(DB_QUERY_REGEXP, '_db_query_callback', $query);
+  return $query;
+}
+
+/**
+ * Initialize tmtools
+ */
+function _tmtools_civicrm_initialize() {
+
+  static $initialized;
+
+  if (!$initialized) {
+
+    civicrm_initialize();
+
+    $tmtools_path = realpath(dirname(__FILE__));
+
+    // also fix php include path
+    $include_path = $tmtools_path . '/civicrm' . PATH_SEPARATOR . get_include_path();
+    set_include_path($include_path);
+
+    $template =& CRM_Core_Smarty::singleton();
+
+    $template->template_dir = array_merge(array($tmtools_path . '/civicrm/Smarty/templates'), (array)$template->template_dir);
+    $template->plugins_dir = array_merge(array($tmtools_path . '/civicrm/Smarty/plugins'), (array)$template->plugins_dir);
+
+    $initialized = TRUE;
+  }
+
+  return $initialized;
+}
